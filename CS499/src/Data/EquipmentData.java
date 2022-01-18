e93a4e227bb7cd51bb4457001aae01a331b25db3
Sebi <71719740+sjrl2000@users.noreply.github.com>
2022-01-18 10:36:36 -0600

    Added NetBeans Files

---
+++ CS499/src/Data/EquipmentData.java
@@ -0,0 +1,21 @@
+
+package Data;
+
+import java.util.*;
+
+public class EquipmentData {
+    
+    
+    
+    
+    
+    
+    int vID = 0;
+    String vBrand = "null";
+    String vYear = "null";
+    String vModel = "null";
+    String vType = "null";
+    List<String> vPartsList = new ArrayList<>();
+    
+    
+}
